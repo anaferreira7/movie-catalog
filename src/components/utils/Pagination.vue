357df9e17435e43351e899fbe55552dfcd459066
Ana Ferreira <ana.ferreira@ludustechnology.com>
2025-03-16 21:11:11 +0000

    Update pagination UI

--- src/components/utils/Pagination.vue
+++ src/components/utils/Pagination.vue
@@ -1,72 +1,51 @@
 <template>
-  <div
-    class="flex justify-between items-center p-4 bg-gray-800 text-white rounded"
-  >
-    <!-- Page Navigation -->
-    <div class="flex items-center space-x-2">
+  <!-- Page Navigation -->
+  <div class="flex items-center justify-center space-x-2 mt-4">
+    <button
+      @click="changePage(1)"
+      :disabled="currentPage === 1"
+      class="px-4 py-2 rounded-full border border-[#BA64FE] text-white transition duration-200 ease-in-out hover:bg-[#BA64FE] hover:border-[#BA64FE] disabled:opacity-50"
+    >
+      &lt;&lt;
+    </button>
+
+    <button
+      @click="changePage(currentPage - 1)"
+      :disabled="currentPage === 1"
+      class="px-4 py-2 rounded-full border border-[#BA64FE] text-white transition duration-200 ease-in-out hover:bg-[#BA64FE] hover:border-[#BA64FE] disabled:opacity-50"
+    >
+      &lt;
+    </button>
+
+    <span v-for="page in visiblePages" :key="page">
       <button
-        @click="changePage(1)"
-        :disabled="currentPage === 1"
-        class="px-3 py-1 bg-gray-700 rounded disabled:opacity-50"
+        @click="changePage(page)"
+        :class="[
+          'px-4 py-2 rounded-full border transition duration-200 ease-in-out',
+          page === currentPage
+            ? 'bg-[#BA64FE] border-[#BA64FE]'
+            : 'border-[#BA64FE] text-white hover:bg-[#BA64FE]',
+        ]"
       >
-        First
+        {{ page }}
       </button>
+    </span>
 
-      <button
-        @click="changePage(currentPage - 1)"
-        :disabled="currentPage === 1"
-        class="px-3 py-1 bg-gray-700 rounded disabled:opacity-50"
-      >
-        Prev
-      </button>
+    <button
+      @click="changePage(currentPage + 1)"
+      :disabled="currentPage === totalPages"
+      class="px-4 py-2 rounded-full border border-[#BA64FE] text-white transition duration-200 ease-in-out hover:bg-[#BA64FE] hover:border-[#BA64FE] disabled:opacity-50"
+    >
+      &gt;
+    </button>
 
-      <span v-for="page in visiblePages" :key="page">
-        <button
-          @click="changePage(page)"
-          :class="[
-            'px-3 py-1 rounded',
-            page === currentPage ? 'bg-blue-500' : 'bg-gray-700',
-          ]"
-        >
-          {{ page }}
-        </button>
-      </span>
-
-      <button
-        @click="changePage(currentPage + 1)"
-        :disabled="currentPage === totalPages"
-        class="px-3 py-1 bg-gray-700 rounded disabled:opacity-50"
-      >
-        Next
-      </button>
-
-      <button
-        @click="changePage(totalPages)"
-        :disabled="currentPage === totalPages"
-        class="px-3 py-1 bg-gray-700 rounded disabled:opacity-50"
-      >
-        Last
-      </button>
-    </div>
-
-    <!-- Movies Per Page Dropdown -->
-    <!-- <div>
-      <label for="perPage" class="mr-2">Movies per page:</label>
-      <select
-        id="perPage"
-        v-model="moviesPerPage"
-        @change="updateMoviesPerPage"
-        class="p-2 bg-gray-700 border border-gray-500 rounded"
-      >
-        <option
-          v-for="option in [10, 20, 30, 50]"
-          :key="option"
-          :value="option"
-        >
-          {{ option }}
-        </option>
-      </select>
-    </div> -->
+    <button
+      @click="changePage(totalPages)"
+      :disabled="currentPage === totalPages"
+      class="px-4 py-2 rounded-full border border-[#BA64FE] text-white transition duration-200 ease-in-out hover:bg-[#BA64FE] hover:border-[#BA64FE] disabled:opacity-50"
+    >
+      &gt;&gt;
+    </button>
   </div>
 </template>
 
@@ -76,28 +55,17 @@ export default {
   props: {
     currentPage: Number,
     totalPages: Number,
-    // perPage: Number,
   },
   computed: {
     visiblePages() {
       const pages = [];
       let start = Math.max(1, this.currentPage - 2);
       let end = Math.min(this.totalPages, this.currentPage + 2);
-
       for (let i = start; i <= end; i++) {
         pages.push(i);
       }
-
       return pages;
     },
-    // moviesPerPage: {
-    //   get() {
-    //     return this.perPage;
-    //   },
-    //   set(value) {
-    //     this.$emit("update:perPage", Number(value));
-    //   },
-    // },
   },
   methods: {
     changePage(page) {
@@ -105,9 +73,12 @@ export default {
         this.$emit("update:currentPage", page);
       }
     },
-    // updateMoviesPerPage() {
-    //   this.$emit("update:perPage", this.moviesPerPage);
-    // },
   },
 };
 </script>
+
+<style scoped>
+button {
+  cursor: pointer;
+}
+</style>
